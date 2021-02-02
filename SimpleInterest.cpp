362e459d4ef2347d45655917c382515ffeeb8f05
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-03 00:30:29 +0530

    Simple interest with arguments

---
+++ SimpleInterest.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void interest(int p,int r,int t);
+float SI;
+int main(){
+interest(30000,4,1);
+cout<<"simple interest:"<<SI;
+}
+void interest(int p,int r,int t){
+
+SI=p*r*t;
+
+
+}
+
+
+
+
+
+
+
