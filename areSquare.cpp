c300d98b4505b9b16e680660b5e697ee6c9e4cac
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-03 23:41:36 +0530

    square with arg and return

---
+++ areSquare.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int square(int a);//a*a
+int Sqr1;
+ int main()
+ {
+square(5);
+cout<<"square is:"<<Sqr1;
+ }
+ int square(int a){
+
+//int a=5;
+Sqr1=a*a;
+return Sqr1;
+
+ }
