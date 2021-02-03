283f45d49147434c4ab148bbde59b937fa4cb384
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-03 23:40:37 +0530

    squre with return no arg

---
+++ square12.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int square();//a*a
+int Sqr1;
+ int main()
+ {
+square();
+cout<<"square is:"<<Sqr1;
+ }
+ int square(){
+
+int a=5;
+Sqr1=a*a;
+return Sqr1;
+
+ }
