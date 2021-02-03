c90816ef33fd5bb6de9a2dbc04141b68f28e3588
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-03 23:24:59 +0530

    area of circle with returntype no arg

---
+++ cirleArea.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int area();
+int main()
+{
+    
+    area();
+}
+int area(){
+
+float pi=3.14; int r=4;
+int c=pi*r*r;
+cout<<"area of circle:"<<""<<c;
+return c;
+}
