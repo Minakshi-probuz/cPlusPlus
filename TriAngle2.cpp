1cfdc0b848f11a1883a8b5999c7f8edfa9e80430
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-03 23:32:39 +0530

    with rreturn and arg area of triangle

---
+++ TriAngle2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int triangle();
+
+
+int triangle(){
+int a=50;
+int b=20;
+float m=0.5;
+    
+    int Area_Trinagle=m*a*b;
+cout<<"Area of triangle is:"<<""<<Area_Trinagle;
+return Area_Trinagle;
+}
+
+int main()
+{
+    triangle();
+    
+}
