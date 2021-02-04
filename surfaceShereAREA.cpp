1541e0a945ac1286ebdfdad339762804d9b3ef1a
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-04 08:39:54 +0530

    surface area sphere by return without argument

---
+++ surfaceShereAREA.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int spher();//4*pi*r
+float sp;
+int main(){
+    spher();
+    cout<<"Area of sphere:"<<sp;
+
+}
+int spher()
+{ int r=2;
+    float pi=3.14;
+    sp=4*pi*r;
+    return sp;
+}
