6834ec3fb2d33e2e0121a2a52cbc899623832ec7
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-06 15:30:06 +0530

    default constructor

---
+++ defaultConstructor.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class test{
+    public:
+    
+    test(){
+        cout<<"this is default constructor"<<endl;
+    }
+};
+int main(){
+    test t,t2,t3;
+}
