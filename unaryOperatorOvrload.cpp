e842097c904bbb08e552889e3500aa10e6b66a04
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-09 23:35:22 +0530

    unary operator overloading

---
+++ unaryOperatorOvrload.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class test{
+    int a;
+    public:
+    test(){
+        a=10;
+    }
+    void operator ++(){
+       ++a;
+    }
+    void operator --(){
+       -- a;
+    }
+    void show(){
+        cout<<"a="<<a<<endl;
+    }
+};
+int main(){
+    test t;
+    ++t;
+    t.show();
+   --t;
+    t.show();
+}
+
