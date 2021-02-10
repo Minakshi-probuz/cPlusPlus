add2e25c55425ae6955f4ee494aaea558ad4885f
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-10 23:06:36 +0530

    pattern 4

---
+++ pattern279.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class pattern3{
+    public:
+    int lines=4;
+    int count=5;
+    char num='A';
+    void set()
+    {
+    for(int i=1;i<=lines;i++)
+    {
+        for(int j=1;j<=count;j++ )
+        {
+
+            cout<<num;
+        }
+        cout<<endl;
+        num++;
+    }
+    }
+};
+int main(){
+    pattern3 p;
+    p.set();
+}
