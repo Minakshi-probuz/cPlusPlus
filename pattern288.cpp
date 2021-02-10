a507e6bb3451298797caef27e5a89cadf5a1a1b1
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-10 23:14:31 +0530

    pattern 12

---
+++ pattern288.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class pattern3{
+    public:
+    int lines=4;
+    int count=1;
+    int num=1;
+    void set()
+    {
+    for(int i=1;i<=lines;i++)
+    {
+        for(int j=1;j<=i;j++ )
+        {
+
+            cout<<num;
+
+        }
+        cout<<endl;
+       num++;
+     // count++;
+    }
+    }
+};
+int main(){
+    pattern3 p;
+    p.set();
+}
