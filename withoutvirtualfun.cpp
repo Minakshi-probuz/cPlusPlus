27d1d97507c15a5161f8e27dfc0b5329ad5d6685
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-12 10:24:32 +0530

    without virtual function

---
+++ withoutvirtualfun.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class one{
+    public:
+    void dis(){
+        cout<<"hello";
+    }
+};
+class two:public one{
+
+public:
+    void dis(){
+        cout<<"minu";
+
+}
+};
+int main(){
+one ob,*obpt;
+obpt=&ob;
+obpt->dis();
+two t;
+obpt=&t;
+obpt->dis();
+
+
+}
