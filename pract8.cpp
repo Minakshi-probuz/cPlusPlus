1e46233c6b2826348bcd7d2e8972b7a24c25d89e
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-15 10:22:17 +0530

    123 456 789

---
+++ pract8.cpp
@@ -0,0 +1,28 @@
+/*
+123
+456
+789
+*/
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n=1;
+  for(int i=1;i<=3;i++){
+
+      for(int j=1;j<=3;j++){
+
+          cout<<n<<" ";
+          n++;
+          
+      }
+      cout<<endl;
+      //++;
+  }
+
+
+
+}
