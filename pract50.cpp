4420d704b624c4cad407284b0329ba98659c787c
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-19 10:49:50 +0530

    4444444 33333 222 1 pattern

---
+++ pract50.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+     int line=4;
+    
+    int space=3;
+    int z;
+    z=line*2-1;
+    int num=4;
+    for(int i=4;i>=1;i--){
+   
+   
+        for(int j=space;j>=i;j--){
+
+            cout<<" ";
+        
+
+           
+        }for(int k=1;k<=z;k++){
+            cout<<num;
+        }
+        cout<<endl;
+       z-=2;
+       num-=1; 
+       
+      
+        
+        
+    }
+}
