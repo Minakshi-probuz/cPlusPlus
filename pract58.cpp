d7dca86e7dc618d9c867734ab466e07cee28d380
Minakshi-probuz <78415026+Minakshi-probuz@users.noreply.github.com>
2021-02-19 10:56:08 +0530

    right hand pyramid of numbers

---
+++ pract58.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+     int line=7;
+    int num=3;
+    int space=3;
+
+    
+    
+    for(int i=3;i>=-3;i--){
+   
+   
+        for(int j=3;j>=abs(i);j--){
+
+            cout<<j;
+        
+
+           
+        }
+        cout<<endl;
+    
+       //ch='D'; 
+       
+      
+        
+        
+    }
+}
